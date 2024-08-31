6698ac7a4193803933cae393317c642d229a303f
khushal <khushalbhasin4488@gmail.com>
2024-08-31 12:00:04 +0530

    changes

--- ws_socket_backend/dist/service/redisService.js
+++ ws_socket_backend/dist/service/redisService.js
@@ -1,31 +1,6 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config();
-const ioredis_1 = require("ioredis");
-const sub = new ioredis_1.Redis(process.env.REDIS_URL.toString());
-const pub = new ioredis_1.Redis(process.env.REDIS_URL.toString());
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield sub.set("hello", "world");
-        yield sub.subscribe("abc", (err, count) => {
-            if (err) {
-                console.log(err);
-            }
-            console.log(count);
-        });
-        sub.on("message", (channel, message) => {
-            console.log(channel, message);
-        });
-    });
+exports.RedisManager = void 0;
+class RedisManager {
 }
-main();
-// because it's not in the subscriber mode.
+exports.RedisManager = RedisManager;
--- ws_socket_backend/src/service/redisService.ts
+++ ws_socket_backend/src/service/redisService.ts
@@ -1,25 +1,3 @@
-require("dotenv").config()
-import { Redis } from "ioredis";
-const sub = new Redis(process.env.REDIS_URL!.toString());
-const pub = new Redis(process.env.REDIS_URL!.toString());
-async function main(){
-
-  await sub.set("hello", "world")
-  await sub.subscribe("abc", (err, count)=>{
-    if(err){
-      console.log(err)
-    }
-    console.log(count)
-  })
-  sub.on("message", (channel, message)=>{
-    console.log(channel, message)
-  })
-
-
-
-
+export class RedisManager {
+  public static instance : RedisManager
 }
-main()
-
-
-  // because it's not in the subscriber mode.
